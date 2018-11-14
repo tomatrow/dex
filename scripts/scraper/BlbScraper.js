93dc1bf72028010e56461dcc7efdb4b89910179f
AJ Caldwell <tomatrow@gmail.com>
2018-11-14 10:53:41 -0800

    Stub out BlbScraper

---
+++ scripts/scraper/BlbScraper.js
@@ -0,0 +1,14 @@
+class BlbScraper {
+    constructor() {}
+    scrapeTextCommenteryIndex(authorName) {
+        return {}
+    }
+    scrapeTextCommenteryBio(authorName) {
+        return {}
+    }
+    scrapeTextCommenterySection(authorName, sectionPath) {
+        return {}
+    }
+}
+
+module.exports = BlbScraper
