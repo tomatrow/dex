52a30e419d4b8ad2a398c793e1f399ef7ec4351c
AJ Caldwell <tomatrow@gmail.com>
2018-11-14 10:53:41 -0800

    Add Context, Goals, and Subgoals for this project

---
+++ scripts/scraper/scrape.js
@@ -0,0 +1,11 @@
+/*
+    Context
+        Scraping blue letter bible text commentaries
+        All commentaries are listed in this index https://www.blueletterbible.org/commentaries/index.cfm
+        Commentaries have varied formats: e.g. an id on one page or new page
+    Goal
+        An athors name returns their corpus of commentaries in json
+    Subgoals
+        given an authors name, return the index of their commentaries
+        given a David Guzik page, return the markup for the notes
+*/
