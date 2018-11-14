222529955c8aa07d213c1a42238f561025e57680
AJ Caldwell <tomatrow@gmail.com>
2018-11-14 10:53:41 -0800

    Add example usage in scrape.js

--- scripts/scraper/scrape.js
+++ scripts/scraper/scrape.js
@@ -9,3 +9,16 @@
         given an authors name, return the index of their commentaries
         given a David Guzik page, return the markup for the notes
 */
+
+
+const BlbScraper = require('./BlbScraper.js')
+const authorName = 'Guzik, David'
+const sectionPath = ['David Guzik Study Guides (2017)', 'Psalms', 'Study Guide for Psalm 23']
+const scraper = new BlbScraper()
+const output = {
+    index: scraper.scrapeTextCommenteryIndex(authorName),
+    bio: scraper.scrapeTextCommenteryBio(authorName),
+    section: scraper.scrapeTextCommenterySection(authorName, sectionPath)
+}
+
+console.log(output)
