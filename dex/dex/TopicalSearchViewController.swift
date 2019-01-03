fb1afb061723598c6b07c198116eaf0eb0badcfc
AJ Caldwell <tomatrow@gmail.com>
2019-01-03 09:59:46 -0800

    Print out search scope/text
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- dex/dex/TopicalSearchViewController.swift
+++ dex/dex/TopicalSearchViewController.swift
@@ -9,82 +9,59 @@
 import UIKit
 
 class TopicalSearchViewController: UITableViewController {
+	lazy var viewModel = ["Hello", "World", "!"]
+	let searchController = UISearchController(searchResultsController: nil)
 
-    override func viewDidLoad() {
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+		// Setup the Search Controller
+		searchController.searchResultsUpdater = self
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.searchBar.placeholder = "Search Topics"
+		navigationItem.searchController = searchController
+		definesPresentationContext = true
+		
+		// Setup the Scope Bar
+		searchController.searchBar.scopeButtonTitles = ["One", "Two", "Three"]
+		searchController.searchBar.delegate = self
+	}
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return viewModel.count
     }
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		
+		let text = viewModel[indexPath.row]
+		cell.textLabel?.text = text
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+		return cell 
+	}
 
-        // Configure the cell...
+}
 
-        return cell
-    }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+extension TopicalSearchViewController: UISearchResultsUpdating {
+	// Note: This is called with the search text changes
+	func updateSearchResults(for searchController: UISearchController) {
+		let searchBar = searchController.searchBar
+		let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+		let text = searchBar.text!
+		print("Searching for '\(text)' in scope: \(scope)")
+	}
+	
+}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension TopicalSearchViewController: UISearchBarDelegate {
+	
 }
