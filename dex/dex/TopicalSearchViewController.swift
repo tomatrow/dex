64bf8ce9caf2bb6383ab8684e0821ee3310ed81b
AJ Caldwell <tomatrow@gmail.com>
2019-01-04 13:19:26 -0800

    Reorganize controller code based on delegates
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- dex/dex/TopicalSearchViewController.swift
+++ dex/dex/TopicalSearchViewController.swift
@@ -9,59 +9,70 @@
 import UIKit
 
 class TopicalSearchViewController: UITableViewController {
-	lazy var viewModel = ["Hello", "World", "!"]
-	let searchController = UISearchController(searchResultsController: nil)
+    lazy var viewModel = loadData()
+    let searchController = UISearchController(searchResultsController: nil)
 
-	
-	override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
-		// Setup the Search Controller
-		searchController.searchResultsUpdater = self
-		searchController.obscuresBackgroundDuringPresentation = false
-		searchController.searchBar.placeholder = "Search Topics"
-		navigationItem.searchController = searchController
-		definesPresentationContext = true
-		
-		// Setup the Scope Bar
-		searchController.searchBar.scopeButtonTitles = ["One", "Two", "Three"]
-		searchController.searchBar.delegate = self
-	}
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Topics"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
 
-    // MARK: - Table view data source
+        // Setup the Scope Bar
+        searchController.searchBar.scopeButtonTitles = ["One", "Two", "Three"]
+        searchController.searchBar.delegate = self
+    }
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+// MARK: Helper methods
+
+extension TopicalSearchViewController {
+    func searchFor(_ text: String, in scope: String) {
+        print("Searching for '\(text)' in scope: \(scope)")
+    }
+
+    func loadData() -> [String] {
+        return ["Hello", "World", "!"]
+    }
+}
+
+// MARK: Table view delegate
+
+extension TopicalSearchViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let text = viewModel[indexPath.row]
+        cell.textLabel?.text = text
+
+        return cell
+    }
+}
+
+// MARK: - Table view data source
+
+extension TopicalSearchViewController {
+    override func numberOfSections(in _: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return viewModel.count
     }
-	
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-		
-		let text = viewModel[indexPath.row]
-		cell.textLabel?.text = text
-
-		return cell 
-	}
-
 }
-
 
 extension TopicalSearchViewController: UISearchResultsUpdating {
-	// Note: This is called with the search text changes
-	func updateSearchResults(for searchController: UISearchController) {
-		let searchBar = searchController.searchBar
-		let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
-		let text = searchBar.text!
-		print("Searching for '\(text)' in scope: \(scope)")
-	}
-	
+    // Note: This is called with the search text changes
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+        let text = searchBar.text!
+        searchFor(text, in: scope)
+    }
 }
 
-
-extension TopicalSearchViewController: UISearchBarDelegate {
-	
-}
+extension TopicalSearchViewController: UISearchBarDelegate {}
