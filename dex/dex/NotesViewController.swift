1e3bef0b2007b2b4de4dcf14471aa8fc1a8f9674
AJ Caldwell <tomatrow@gmail.com>
2019-01-05 10:25:16 -0800

    Make notes easily dismissable
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- dex/dex/NotesViewController.swift
+++ dex/dex/NotesViewController.swift
@@ -9,22 +9,9 @@
 import UIKit
 
 class NotesViewController: UIViewController {
-
+    @IBOutlet var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textView.keyboardDismissMode = .onDrag
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
