3efc658dc669a9ff4e9a0ab6a4b519aac1746e81
AJ Caldwell <tomatrow@gmail.com>
2019-01-08 19:52:59 -0800

    Save/Load state of notes
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- dex/dex/NotesViewController.swift
+++ dex/dex/NotesViewController.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class NotesViewController: UIViewController {
+    var notes = NotesViewController.loadText() {
+        didSet {
+            saveText(notes)
+        }
+    }
+
     @IBOutlet var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.keyboardDismissMode = .interactive
+        textView.delegate = self
+        textView.text = notes
+    }
+}
+
+extension NotesViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_: UITextView) {
+        notes = textView.text
+    }
+}
+
+fileprivate let notesKey = "notes-text"
+
+extension NotesViewController {
+    func saveText(_ text: String) {
+        UserDefaults.standard.set(text, forKey: notesKey)
+    }
+
+    static func loadText() -> String {
+        return UserDefaults.standard.string(forKey: notesKey) ?? String(repeating: "\n", count: 100)
     }
 }
