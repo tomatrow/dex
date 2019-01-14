631a46713c82de722650194b5e2b08b9bb1630c4
AJ Caldwell <tomatrow@gmail.com>
2019-01-14 12:51:03 -0800

    Change root controller to Journal
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- dex/dex/AppDelegate.swift
+++ dex/dex/AppDelegate.swift
@@ -13,13 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let tabBarController = window!.rootViewController as! UITabBarController
-
-        // Override point for customization after application launch.
-        let splitViewController = tabBarController.viewControllers!.filter { $0 is UISplitViewController }.first! as! UISplitViewController
-        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count - 1] as! UINavigationController
-        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
-        splitViewController.delegate = self
         return true
     }
 
@@ -29,17 +22,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(_: UIApplication, shouldRestoreApplicationState _: NSCoder) -> Bool {
         return true
-    }
-
-    // MARK: - Split view
-
-    func splitViewController(_: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto _: UIViewController) -> Bool {
-        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
-        return false
     }
 }
