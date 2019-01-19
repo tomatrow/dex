9b4fbafd808e26ec521e3c1f069a2a44515979e0
AJ Caldwell <tomatrow@gmail.com>
2019-01-18 21:49:38 -0800

    Add Bible controller
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

---
+++ dex/dex/Controller/BibleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BibleViewController.swift
+//  dex
+//
+//  Created by AJ Caldwell on 1/18/19.
+//  Copyright © 2019 optional(default). All rights reserved.
+//
+
+import UIKit
+
+class BibleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
